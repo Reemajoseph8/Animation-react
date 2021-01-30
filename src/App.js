22c07a70e8e6a99a527d0bc7c39b023153b63c88
Reemajoseph8 <reemajoseph888@gmail.com>
2021-01-30 11:38:40 +0530

    bg image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Anime from './anime';
 import { Fade } from "react-reveal";
-import {Container} from "react-bootstrap";
-
+import {Container } from "react-bootstrap";
 function App() {
   return (
     <Container className="text-center" fluid>
+      <div style={{"background-image": "url('https://c4.wallpaperflare.com/wallpaper/246/739/689/digital-digital-art-artwork-illustration-abstract-hd-wallpaper-preview.jpg')",
+      "width":"100%","height":"80%","position":"absolute"}}>
     <div className="center-left">
     <Fade up>
-      <h1 className="heading" style={{"font-size":"80px"}}>
+      <h1 className="heading" style={{"font-size":"80px","color":"white"}}>
         <Anime />
       </h1>
-      <h2 style={{"font-size":"50px","font-weight":"600"}}>
+      <h2 style={{"font-size":"50px","font-weight":"600","color":"white"}}>
       I create value through design <br></br>
 and illustration to elevate your<br></br> company to a new level.
       </h2>
     </Fade>
+  </div>
   </div>
 </Container>
     
